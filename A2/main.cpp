df218bc19dfbd09518e4ecf8102e718d21cf2fa2
Rahma Nader <rahmanaderm@gmail.com>
2022-05-08 21:47:30 +0200

    added Hermite curve function

--- A2/main.cpp
+++ A2/main.cpp
@@ -20,6 +20,29 @@ int Round(double x)
 {
 	return (int)(x + 0.5);
 }
+struct Vector{
+	double v[2];
+	Vector(double x = 0, double y = 0)
+	 { v[0] = x; v[1] = y; }
+	double& operator[](int i){ return v[i]; 
+	}
+};
+void DrawHermiteCurve(HDC hdc,Vector& p1, Vector& T1, Vector& p2, Vector& T2, COLORREF c)
+{
+	double a0 = p1[0], a1 = T1[0],
+		a2 = -3 * p1[0] - 2 * T1[0] + 3 * p2[0] - T2[0],
+		a3 = 2 * p1[0] + T1[0] - 2 * p2[0] + T2[0];
+	double b0 = p1[1], b1 = T1[1],
+		b2 = -3 * p1[1] - 2 * T1[1] + 3 * p2[1] - T2[1],
+		b3 = 2 * p1[1] + T1[1] - 2 * p2[1] + T2[1];
+	for (double t = 0; t <= 1; t += 0.001)
+	{
+		double t2 = t*t, t3 = t2*t;
+		double x = a0 + a1*t + a2*t2 + a3*t3;
+		double y = b0 + b1*t + b2*t2 + b3*t3;
+		SetPixel(hdc, Round(x), Round(y), c);
+	}
+}
 void DrawLine1(HDC hdc, int x1, int y1, int x2, int y2, COLORREF c)
 {
 	int dx = x2 - x1;
